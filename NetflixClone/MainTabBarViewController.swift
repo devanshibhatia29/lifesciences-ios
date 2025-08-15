a9c8d68600fd5bf3a50a3186ddda25e907bd8c10
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-15 20:03:53 +0530

    Update MainTabBarViewController.swift

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -17,30 +17,6 @@ class MainTabBarViewController: UITabBarController {
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadViewController())
         
-vc4.title = "Begin sync"
-```
-
-### File: NetflixClone/MainTabBarViewController.swift
-```swift
-//
-//  ViewController.swift
-//  NetflixClone
-//
-//  Created by Vasu Chand on 16/10/24.
-//
-
-import UIKit
-
-class MainTabBarViewController: UITabBarController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let vc1 = UINavigationController(rootViewController: HomeViewController())
-        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
-        let vc3 = UINavigationController(rootViewController: SearchViewController())
-        let vc4 = UINavigationController(rootViewController: DownloadViewController())
-        
         vc1.tabBarItem.image = UIImage(systemName: "hosue")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
@@ -56,17 +32,15 @@ class MainTabBarViewController: UITabBarController {
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 }
-```
+
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-        
-        vc1.title = "Netflix"
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
 
-        
+        vc1.title = "Netflix"
         vc2.title = "Incoming"
         vc3.title = "Top Search"
-        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
-        vc4.title = "Sync"
+        vc4.title = "Download Account"
         
         tabBar.tintColor = .label
         
