52b4f637baabf90aee14cb3fffeab654bd901f6b
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-15 19:54:33 +0530

    Fix bug: Label not as per CX doc

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -27,7 +27,46 @@ class MainTabBarViewController: UITabBarController {
         vc2.title = "Incoming"
         vc3.title = "Top Search"
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
-        vc4.title = "Sync"
+        vc4.title = "Begin sync"
+```
+
+However, since the file content is truncated, here's the complete corrected section based on the visible pattern:
+
+### File: NetflixClone/MainTabBarViewController.swift
+```swift
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Vasu Chand on 16/10/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Netflix"
+        vc2.title = "Incoming"
+        vc3.title = "Top Search"
+        vc4.title = "Begin sync"
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+    }
+}
+```
         
         tabBar.tintColor = .label
         
