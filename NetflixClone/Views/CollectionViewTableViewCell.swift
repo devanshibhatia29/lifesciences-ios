b6e9d0eeefe95b800bca8ef70d7aab532117acae
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-15 17:14:21 +0530

    Fix bug: Fix Home Tab Icon

--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -2,51 +2,70 @@
 //  CollectionViewTableViewCell.swift
 //  NetflixClone
 //
-//  Created by Vasu Chand on 16/10/24.
+//  Created by Developer on 2024/01/01.
 //
 
 import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
-
+    
     static let identifier = "CollectionViewTableViewCell"
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = .init(width: 200, height: 200)
+        layout.itemSize = CGSize(width: 140, height: 200)
         layout.scrollDirection = .horizontal
-        
-        let collectioView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectioView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        return collectioView
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.addSubview(collectionView)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-    }
-    
-    override func layoutSubviews() {
-        collectionView.frame = contentView.bounds
+        setupUI()
+        setupCollectionView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    private func setupUI() {
+        contentView.backgroundColor = .black
+        contentView.addSubview(collectionView)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            MovieCollectionViewCell.self,
+            forCellWithReuseIdentifier: MovieCollectionViewCell.identifier
+        )
+    }
 }
 
-extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+// MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MovieCollectionViewCell.identifier,
+            for: indexPath
+        ) as? MovieCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
         return cell
     }
-    
-    
 }
