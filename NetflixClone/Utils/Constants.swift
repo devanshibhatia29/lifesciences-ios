504adc385b062386d5b767930c1485cd1a645764
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-15 16:55:29 +0530

    Fix bug: Change Button Color

---
+++ NetflixClone/Utils/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  NetflixClone
+//
+//  Created by Developer on 12/15/2024.
+//
+
+import UIKit
+
+struct AppColors {
+    static let primaryRed = UIColor(red: 229/255, green: 9/255, blue: 20/255, alpha: 1.0)
+    static let primaryBlue = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.0) // #007AFF
+    static let backgroundColor = UIColor.black
+    static let secondaryTextColor = UIColor.lightGray
+    static let whiteColor = UIColor.white
+}
+
+struct AppConstants {
+    static let cornerRadius: CGFloat = 8.0
+    static let buttonHeight: CGFloat = 44.0
+    static let defaultPadding: CGFloat = 16.0
+}
