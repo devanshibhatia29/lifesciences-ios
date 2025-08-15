2fff523cbd32141391abe0e016b15adac56be294
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-15 22:11:05 +0530

    Fix bug: Cannot see new button to create CPA for certain accounts

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -17,7 +17,7 @@ class MainTabBarViewController: UITabBarController {
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadViewController())
         
-        vc1.tabBarItem.image = UIImage(systemName: "hosue")
+        vc1.tabBarItem.image = UIImage(systemName: "house")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
@@ -25,13 +25,10 @@ class MainTabBarViewController: UITabBarController {
         vc1.title = "Netflix"
         vc2.title = "Incoming"
         vc3.title = "Top Search"
-        vc4.title = "Download Account"
+        vc4.title = "Downloads"
         
         tabBar.tintColor = .label
         
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
-
-
 }
-
